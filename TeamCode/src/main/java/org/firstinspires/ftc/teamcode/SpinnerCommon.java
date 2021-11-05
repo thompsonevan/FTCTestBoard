87ac1931f29de942ac73be3fb46a6c7f257d5f44
thompsonevan <evan.thompson515@gmail.com>
2021-11-05 17:13:51 -0400

    change spinner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinnerCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinnerCommon.java
@@ -14,16 +14,20 @@ public class SpinnerCommon {
     }
 
     public void excuteTeleop(){
-        boolean CCW = curOpMode.gamepad1.x;
-        boolean CW = curOpMode.gamepad1.b;
+//        boolean CCW = curOpMode.gamepad1.x;
+//        boolean CW = curOpMode.gamepad1.b;
+//
+//        if(CCW == true) {
+//            robot.spinnerMotor.setPower(-1);
+//        } else if (CW){
+//            robot.spinnerMotor.setPower(1);
+//        } else {
+//            robot.spinnerMotor.setPower(0);
+//        }
 
-        if(CCW == true) {
-            robot.spinnerMotor.setPower(-1);
-        } else if (CW){
-            robot.spinnerMotor.setPower(1);
-        } else {
-            robot.spinnerMotor.setPower(0);
-        }
+        robot.spinnerMotor.setPower(-curOpMode.gamepad1.left_trigger);
+        robot.spinnerMotor.setPower(curOpMode.gamepad1.right_trigger);
+
 
     }
 }
