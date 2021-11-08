5cdc187c998e7e2229a4d635bb0be31ef617d88b
thompsonevan <evan.thompson515@gmail.com>
2021-11-08 10:27:09 -0500

    not finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtenderHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtenderHardware.java
@@ -11,9 +11,16 @@ package org.firstinspires.ftc.teamcode;
 // This imports all the different dependencies and libraries and other classes we use in this file
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class ExtenderHardware {
+
+    public Servo s1;
+    public Servo s2;
+    public Servo s3;
+    public Servo s4;
+    public Servo s5;
 
     HardwareMap hwMap =  null;
     private ElapsedTime period = new ElapsedTime();
@@ -21,6 +28,11 @@ public class ExtenderHardware {
     public void init(HardwareMap ahwMap) {
         hwMap = ahwMap;
 
+        s1 = hwMap.get(Servo.class, "s1");
+        s2 = hwMap.get(Servo.class, "s2");
+        s3 = hwMap.get(Servo.class, "s3");
+        s4 = hwMap.get(Servo.class, "s4");
+        s5 = hwMap.get(Servo.class, "s5");
     }
  }
 
