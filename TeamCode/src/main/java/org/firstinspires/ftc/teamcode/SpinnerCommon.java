81fd51118edd83f3712e897d0d9f4de062386a60
thompsonevan <evan.thompson515@gmail.com>
2021-11-08 09:46:43 -0500

    fix from merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinnerCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinnerCommon.java
@@ -14,18 +14,6 @@ public class SpinnerCommon {
     }
 
     public void excuteTeleop(){
-//        boolean CCW = curOpMode.gamepad1.x;
-//        boolean CW = curOpMode.gamepad1.b;
-//
-//        if(CCW == true) {
-//            robot.spinnerMotor.setPower(-1);
-//        } else if (CW){
-//            robot.spinnerMotor.setPower(1);
-//        } else {
-//            robot.spinnerMotor.setPower(0);
-//        }
-
-
         if(curOpMode.gamepad1.left_trigger > .05){
             robot.spinnerMotor.setPower(-curOpMode.gamepad1.left_trigger);
         } else {
