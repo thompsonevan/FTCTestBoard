c8b165f439701d2f526c0c8c2ff9ddd7e33f1192
Evan Thompson <46571663+thompsonevan@users.noreply.github.com>
2021-11-08 13:06:28 -0500

    Might Work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtenderCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtenderCommon.java
@@ -40,15 +40,55 @@ public class ExtenderCommon {
                 s1Val = setVal(s1Val, .01f);
             }
             while(curOpMode.gamepad1.right_trigger > .1){
-                s1Val -= .01;
+                s1Val = setVal(s1Val, -.01f);
             }
             robot.s1.setPosition(s1Val);
+        } else if (state == 1){
+            while(curOpMode.gamepad1.left_trigger > .1){
+                s2Val = setVal(s2Val, .01f);
+            }
+            while(curOpMode.gamepad1.right_trigger > .1){
+                s2Val = setVal(s2Val, -.01f);
+            }
+            robot.s2.setPosition(s2Val);
+        } else if (state == 2){
+            while(curOpMode.gamepad1.left_trigger > .1){
+                s3Val = setVal(s3Val, .01f);
+            }
+            while(curOpMode.gamepad1.right_trigger > .1){
+                s3Val = setVal(s3Val, -.01f);
+            }
+            robot.s3.setPosition(s3Val);
+        } else if (state == 3){
+            while(curOpMode.gamepad1.left_trigger > .1){
+                s4Val = setVal(s4Val, .01f);
+            }
+            while(curOpMode.gamepad1.right_trigger > .1){
+                s4Val = setVal(s4Val, -.01f);
+            }
+            robot.s4.setPosition(s4Val);
+        } else if (state == 4){
+            while(curOpMode.gamepad1.left_trigger > .1){
+                s5Val = setVal(s5Val, .01f);
+            }
+            while(curOpMode.gamepad1.right_trigger > .1){
+                s5Val = setVal(s5Val, -.01f);
+            }
+            robot.s5.setPosition(s5Val);
         }
 
     }
 
     public float setVal(float curVal, float changeVal){
-        return .01f;
+        float returnVal = 0;
+        
+        if (curVal + changeVal > 1 || curVal + changeVal < 0){
+            returnVal = curVal;
+        } else {
+            returnVal = curVal + changeVal;
+        }
+        
+        return returnVal;
     }
 
 }
