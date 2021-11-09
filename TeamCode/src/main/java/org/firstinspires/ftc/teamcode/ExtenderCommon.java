1e6013d7940b50ef0adcdd9b4ad32c7dfa749dab
Evan Thompson <evan.thompson515@gmail.com>
2021-11-09 07:53:38 -0500

    Revert "not finished"
    This reverts commit 5cdc187c998e7e2229a4d635bb0be31ef617d88b.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtenderCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtenderCommon.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class ExtenderCommon {
 
@@ -9,46 +8,12 @@ public class ExtenderCommon {
 
     public LinearOpMode curOpMode;
 
-    public float s1Val = 0;
-    public float s2Val = 0;
-    public float s3Val = 0;
-    public float s4Val = 0;
-    public float s5Val = 0;
-
-    public int state = 0;
-
     public ExtenderCommon(LinearOpMode owningOpMode){
         curOpMode = owningOpMode;
         robot.init(curOpMode.hardwareMap);
     }
 
     public void excuteTeleop(){
-        if(curOpMode.gamepad1.start){
-            state = 0;
-        } else if (curOpMode.gamepad1.b){
-            state = 1;
-        } else if (curOpMode.gamepad1.x){
-            state = 2;
-        } else if (curOpMode.gamepad1.y){
-            state = 3;
-        } else if (curOpMode.gamepad1.a){
-            state = 4;
-        }
-
-        if(state == 0){
-            while(curOpMode.gamepad1.left_trigger > .1){
-                s1Val = setVal(s1Val, .01f);
-            }
-            while(curOpMode.gamepad1.right_trigger > .1){
-                s1Val -= .01;
-            }
-            robot.s1.setPosition(s1Val);
-        }
 
     }
-
-    public float setVal(float curVal, float changeVal){
-        return .01f;
-    }
-
 }
