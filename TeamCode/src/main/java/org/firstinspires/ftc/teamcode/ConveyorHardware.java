604e06228c9b7aea337bbca56c4b2bfe09231f0f
Evan Thompson <evan.thompson515@gmail.com>
2021-11-09 07:53:47 -0500

    Revert "move sensor data"
    This reverts commit a78e929accaf49da6d57bf577e343f4814957bef.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConveyorHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConveyorHardware.java
@@ -21,9 +21,6 @@ public class ConveyorHardware {
     public Servo conveyorServo;
     public DcMotor conveyorMotor;
 
-    public DistanceSensor ds1;
-    public DistanceSensor ds2;
-
     HardwareMap hwMap =  null;
     private ElapsedTime period = new ElapsedTime();
 
@@ -40,9 +37,6 @@ public class ConveyorHardware {
         conveyorMotor.setPower(0);
 
         conveyorServo.setPosition(0);
-
-        ds1 = hwMap.get(DistanceSensor.class, "ds1");
-        ds2 = hwMap.get(DistanceSensor.class, "ds2");
     }
 }
 
